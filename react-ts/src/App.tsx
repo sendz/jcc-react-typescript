a57f157231a05bebc8b426afc4454ddc8aa2d125
Sendy Aditya Suryana <sendzation@gmail.com>
2021-12-29 12:05:01 +0700

    Refactor directory

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { ToDoListForm } from './components/todo-list/Form';
+import { ToDoListForm } from './components/todo-list/form';
 import { ToDoList } from './components/todo-list/List';
 import { ToDoListProvider } from './providers/ToDoList.provider';
 
