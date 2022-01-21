dc0186bba2949b2c306701f59bf7afa960572f20
Sendy Aditya Suryana <sendzation@gmail.com>
2022-01-21 21:17:28 +0700

    Remove unused import

--- react-pwa/src/App.tsx
+++ react-pwa/src/App.tsx
@@ -1,41 +1,43 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [age, setAge] = useState(0)
+  const [todos, setTodos] = useState<any[]>()
   const [message, setMessage] = useState<string>()
   useEffect(() => {
     (async () => {
       const response = await fetch('http://localhost:1234/todo')
-      console.log("RESPONSE", response.json())
+      const data = await response.json()
+      console.log(data.data)
+      setTodos(data.data)
     })()
-  })
+  }, [])
 
   navigator.serviceWorker.onmessage = (event) => {
     console.log("MESSAGE FROM SERVICE WORKER", event.data)
+    if (event.data.type === "NEW_TODO") {
+      const newTodos = [
+        ...todos!,
+        event.data.payload
+      ]
+      setTodos(newTodos)
+      return setMessage(`New ${event.data.type} with ${event.data.payload.title} message`)
+    }
     setMessage(`New ${event.data.type} with ${event.data.payload} message`)
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         {message && (
           <p>{message}</p>
         )}
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <ul>
+        {todos?.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
